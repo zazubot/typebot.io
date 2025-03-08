49ee92fa122566361cb6bad64acdd1ca67e7d516
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2025-03-08 13:57:44 +0200

    LoginCodeEmail

--- packages/transactional/emails/LoginCodeEmail.tsx
+++ packages/transactional/emails/LoginCodeEmail.tsx
@@ -33,19 +33,19 @@ interface Props {
 export const LoginCodeEmail = ({ url, code }: Props) => (
   <Html>
     <Head />
-    <Preview>Your login code for Typebot</Preview>
+    <Preview>Your login code for Zazubot</Preview>
     <Body style={main}>
       <Container style={container}>
         <Img
           src={`${env.NEXTAUTH_URL}/images/logo.png`}
           width="32"
           height="32"
-          alt="Typebot's Logo"
+          alt="Zazubot's Logo"
           style={{
             margin: "24px 0",
           }}
         />
-        <Heading style={heading}>Your login code for Typebot</Heading>
+        <Heading style={heading}>Your login code for Zazubot</Heading>
         <code style={codeStyle}>{code}</code>
         <Text style={paragraph}>
           This code will only be valid for the next 5 minutes.
@@ -54,7 +54,7 @@ export const LoginCodeEmail = ({ url, code }: Props) => (
           You can also sign in by <Link href={url}>clicking here</Link>.
         </Text>
         <Hr style={hr} />
-        <Text style={footerText}>Typebot - Build faster, Chat smarter</Text>
+        <Text style={footerText}>Zazubot - Build faster, Chat smarter</Text>
       </Container>
     </Body>
   </Html>

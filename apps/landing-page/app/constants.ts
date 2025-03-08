22fe720fcfd7e02a067ce46d3e4a768f706f25a0
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2025-03-08 04:31:14 +0200

    zazubot.com

--- apps/landing-page/app/constants.ts
+++ apps/landing-page/app/constants.ts
@@ -3,13 +3,13 @@ export const breakpoints = {
 };
 
 export const currentBaseUrl = "https://typebot.io";
-export const signinUrl = "https://app.typebot.io/signin";
-export const registerUrl = "https://app.typebot.io/register";
+export const signinUrl = "https://app.zazubot.com/signin";
+export const registerUrl = "https://app.zazubot.com/register";
 export const githubRepoUrl = "https://github.com/baptisteArno/typebot.io";
 export const blueskyUrl = "https://bsky.app/profile/typebot.io";
 export const linkedInUrl = "https://www.linkedin.com/company/typebot";
 export const discordUrl = "https://typebot.io/discord";
-export const docsUrl = "https://docs.typebot.io";
+export const docsUrl = "https://docs.zazubot.com";
 export const howToGetHelpUrl = `${docsUrl}/guides/how-to-get-help`;
 export const stripeClimateUrl = "https://climate.stripe.com/5VCRAq";
 export const enterpriseLeadTypebotUrl =

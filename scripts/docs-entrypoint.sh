dd17449569ea93ad13d8928f6c4615574acc840c
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2025-03-10 14:46:55 +0200

    remove docs

--- scripts/docs-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd apps/docs;
-node  -e "const { configureRuntimeEnv } = require('next-runtime-env/build/configure'); configureRuntimeEnv();"
-cd ../..;
-
-NODE_OPTIONS=--no-node-snapshot HOSTNAME=0.0.0.0 PORT=3000 node apps/docs/server.js;
